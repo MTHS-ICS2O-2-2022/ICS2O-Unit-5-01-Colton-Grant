c88f98ff2954156e4262fb1c441c2a40b166c931
Colton-Grant <124389241+Colton-Grant@users.noreply.github.com>
2023-05-29 17:01:44 +0000

    fffd

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Colton
+// Created on: april 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+
+function calculate () {
+  const randomNumber = Math.floor(Math.random() * 6) + 1 // returns a random integer from 1 to 6
+
+/**
+ * This function updates the slider value.
+ */
+  const numberGuessed = parseInt(document.getElementById('guessed-number').value)
+
+  if (numberGuessed == randomNumber) {
+    document.getElementById("answer").innerHTML =
+      "The answer was " + randomNumber + "!" + " You got it right! Good job."
+  }
+
+  if (numberGuessed !== randomNumber) {
+    document.getElementById("answer").innerHTML =
+      "The answer was " + randomNumber + "!" + " You got it wrong! Try again."  
+  }
+}
